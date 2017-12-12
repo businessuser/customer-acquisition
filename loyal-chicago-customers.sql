17b8dac4e39b2ff558fd2ba062821090fad94c68
businessuser <34491215+businessuser@users.noreply.github.com>
2017-12-12 15:28:08 -0600

    Create new file loyal-chicago-customers.sql, specify business requirements.

---
+++ loyal-chicago-customers.sql
@@ -0,0 +1,8 @@
+/* SQL to select customers:
+  - active in the last 6 months
+  - in the Chicago area
+  
+  Output will be used as input for person matching.
+*/
+
+-- TODO: Write query satisfying above requirements.
